3043144d7f9bec30985af3b64d15cbfe8a93b4e3
Ashish Soni <ashish.soni@intelligentMachineslab.com>
2021-05-14 08:54:27 +0530

    Second commit

---
+++ ArrayDistinct.java
@@ -0,0 +1,22 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class ArrayDistinct {
+
+	public static void main(String gg[]) {
+		System.out.println("Wellcome to Array gitpractice");
+		List<String> list = new ArrayList<>();
+		list.add("a");
+		list.add("b");
+		list.add("c");
+		list.add("d");
+		System.out.println("List 1: "+ list);
+		List<String> list2 = list.stream().distinct().collect(Collectors.toList());
+		System.out.println("List 2: "+ list2);
+		list.add("a");
+		System.out.println("List 1.1: " + list);
+		List<String> list3 = list.stream().distinct().collect(Collectors.toList());
+		System.out.println("List 3: " + list3);
+	}
+}
--- ProjectDetails.java
+++ ProjectDetails.java
@@ -1,5 +1,5 @@
 public class ProjectDetails{
 	public static void main(String aa[]){
-		
+		System.out.println("Welcome to gitpractice");
 	}
 }
